541b1a5b79e3a59519cf55f19e7ce6c29f5c11a5
pepiioloxx <160136260+pepiioloxx@users.noreply.github.com>
2024-03-19 08:35:13 +0100

    Create secret.php

---
+++ p5/secret.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Registre d'estudiants</title>
+</head>
+<body>
+	<!-- Menú -->
+	<script type="text/javascript" src="menu.js"></script>
+
+	<h1>Secret</h1>
+
+
+</body>
+</html>
